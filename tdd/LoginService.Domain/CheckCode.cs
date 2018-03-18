3b31a147a353cea95444c2244f4d30d5b072aad3
Dmitry Ivanov <yes2009@gmail.com>
2018-03-18 22:55:32 +0300

    create TDD project and LoginService.Domain

---
+++ tdd/LoginService.Domain/CheckCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginService.Domain
+{
+	public enum CheckCode
+	{
+		Unknown=0,
+		Success =1,
+		TooLong =2,
+		TooShort =3
+	}
+}
---
+++ tdd/LoginService.Tests/LoginTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginService.Tests
+{
+	[TestFixture]
+	public class LoginTests
+    {
+		[Test]
+		public void Check_Success()
+		{
+			//Arrange
+			var login = new LoginService();
+
+			//Act
+
+			//Assert
+		}
+    }
+}
